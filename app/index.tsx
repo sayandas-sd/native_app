6ed4fdbd408a6572c9ccceb6884a4e296e03459a
sayandas-sd <sayandasmac@gmail.com>
2024-09-26 23:56:07 +0530

    added images icon and initial landing page

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Text, View  } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-bold">
+        Native App
+      </Text>
+      <StatusBar style="auto"/>
+      <Link href="/profile">click this</Link>
+    </View>
+  )
+}
---
+++ app/profile.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View  } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>
+        profile
+      </Text>
+      <StatusBar style="auto"/>
+    </View>
+  )
+}
