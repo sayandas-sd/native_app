92d6b015fc2998a84f1a6e380c755f217027153c
sayandas-sd <sayandasmac@gmail.com>
2024-09-27 23:11:30 +0530

    added navigation and different route

--- app/(dashboard)/_layout.tsx
+++ app/(dashboard)/_layout.tsx
@@ -1,16 +1,17 @@
 import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
-
+import { Tabs } from "expo-router";
 
 export default function Auth() {
 
   return (
-    <Stack>
-      <Stack.Screen 
-        options={{headerShown: false}} 
-        name="index"
-      />
-    </Stack>
+    <>
+      <Tabs>
+        <Tabs.Screen
+        name="home" />
+          
+      </Tabs>
+    </>
   )
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,13 +2,8 @@ import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
 
-
-
-
-
 export default function RootApp() {
 
- 
   return (
     <Stack>
       <Stack.Screen 
